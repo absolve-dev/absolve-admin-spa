d912f42e4c3772568391b1e65a9e9c6f51fbfafc
Angelo Lirazan <angelo.g.lirazan@gmail.com>
2016-08-18 04:04:33 -0700

    create tables for catalog database (php artisan make:migration create_catalog_parts)

---
+++ database/migrations/2016_08_18_085412_create_catalog_parts.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCatalogParts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // All of the catalog data created here
+        Schema::create("catalogs", function (Blueprint $table) {
+          $table->increments("id");
+          $table->string("name");
+          $table->string("default_image_url");
+          $table->timestamps();
+        });
+        Schema::create("catalog_sets", function (Blueprint $table) {
+          $table->increments("id");
+          $table->string("name");
+          $table->integer("catalog_id")->unsigned();
+          $table->foreign("catalog_id")
+            ->references("id")->on("catalogs")
+            ->onDelete("cascade");
+          $table->string("default_image_url");
+          $table->timestamps();
+        });
+        Schema::create("catalog_cards", function (Blueprint $table) {
+          $table->increments("id");
+          $table->string("name");
+          $table->integer("catalog_set_id")->unsigned();
+          $table->foreign("catalog_set_id")
+            ->references("id")->on("catalog_sets")
+            ->onDelete("cascade");
+          $table->string("json_data"); // meant for json data
+          $table->string("default_image_url");
+          $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      Schema::drop("catalogs");
+      Schema::drop("catalog_sets");
+      Schema::drop("catalog_cards");
+    }
+}
