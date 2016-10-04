c70494d914f904707447b0166a2eeb0c3d408608
Angelo Lirazan <angelo.g.lirazan@gmail.com>
2016-10-04 16:12:39 -0700

    remove the slashes (potentially causing redirect issues)

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,43 +19,43 @@ Route::get('/admin/', function () {
     return view('admin/index');
 });
 
-Route::group(["prefix" => "/api/v1", "namespace" => "Api\V1"], function(){
+Route::group(["prefix" => "api/v1", "namespace" => "Api\V1"], function(){
 
-  Route::group(["prefix" => "/catalog"], function(){
-    Route::group(["prefix" => "/set"], function(){
-      Route::get("/{catalogSetId}", "CatalogSetController@getShow");
+  Route::group(["prefix" => "catalog"], function(){
+    Route::group(["prefix" => "set"], function(){
+      Route::get("{catalogSetId}", "CatalogSetController@getShow");
     });
-    Route::group(["prefix" => "/item"], function(){
-      Route::get("/{catalogItemId}", "CatalogItemController@getShow");
+    Route::group(["prefix" => "item"], function(){
+      Route::get("{catalogItemId}", "CatalogItemController@getShow");
     });
-    Route::get("/", "CatalogController@getIndex");
-    Route::get("/{catalogId}", "CatalogController@getShow");
+    Route::get("", "CatalogController@getIndex");
+    Route::get("{catalogId}", "CatalogController@getShow");
   });
 
-  Route::group(["prefix" => "/inventory"], function(){
-    Route::group(["prefix" => "/set"], function(){
-      Route::post("/", "InventorySetController@create");
-      Route::get("/{inventorySetId}", "InventorySetController@show");
-      Route::post("/{inventorySetId}", "InventorySetController@update");
-      Route::delete("/{inventorySetId}", "InventorySetController@delete");
+  Route::group(["prefix" => "inventory"], function(){
+    Route::group(["prefix" => "set"], function(){
+      Route::post("", "InventorySetController@create");
+      Route::get("{inventorySetId}", "InventorySetController@show");
+      Route::post("{inventorySetId}", "InventorySetController@update");
+      Route::delete("{inventorySetId}", "InventorySetController@delete");
     });
-    Route::group(["prefix" => "/item"], function(){
-      Route::post("/", "InventoryItemController@create");
-      Route::get("/{inventoryItemId}", "InventoryItemController@show");
-      Route::post("/{inventoryItemId}", "InventoryItemController@update");
-      Route::delete("/{inventoryItemId}", "InventoryItemController@delete");
+    Route::group(["prefix" => "item"], function(){
+      Route::post("", "InventoryItemController@create");
+      Route::get("{inventoryItemId}", "InventoryItemController@show");
+      Route::post("{inventoryItemId}", "InventoryItemController@update");
+      Route::delete("{inventoryItemId}", "InventoryItemController@delete");
     });
-    Route::group(["prefix" => "/listing"], function(){
-      Route::post("/", "InventoryListingController@create");
-      Route::post("/{inventoryListingId}", "InventoryListingController@update");
-      Route::delete("/{inventoryListingId}", "InventoryListingController@delete");
+    Route::group(["prefix" => "listing"], function(){
+      Route::post("", "InventoryListingController@create");
+      Route::post("{inventoryListingId}", "InventoryListingController@update");
+      Route::delete("{inventoryListingId}", "InventoryListingController@delete");
       //Route::get("/{inventoryListingId}", "InventoryListingController@show");
     });
-    Route::get("/", "InventoryController@index");
-    Route::post("/", "InventoryController@create");
-    Route::get("/{inventoryId}", "InventoryController@show");
-    Route::post("/{inventoryId}", "InventoryController@update");
-    Route::delete("/{inventoryId}", "InventoryController@delete");
+    Route::get("", "InventoryController@index");
+    Route::post("", "InventoryController@create");
+    Route::get("{inventoryId}", "InventoryController@show");
+    Route::post("{inventoryId}", "InventoryController@update");
+    Route::delete("{inventoryId}", "InventoryController@delete");
   });
 
 });
