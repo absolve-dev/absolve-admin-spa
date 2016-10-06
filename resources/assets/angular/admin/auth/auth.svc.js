e552782822ba9f4af78ea38b37c7f4a149d8265c
Angelo Lirazan <angelo.g.lirazan@gmail.com>
2016-10-05 17:23:21 -0700

    implement all auth service methods

--- resources/assets/angular/admin/auth/auth.svc.js
+++ resources/assets/angular/admin/auth/auth.svc.js
@@ -1,18 +1,40 @@
 angular.module("absolve.admin.auth")
-  .service("authService", function($http){
-    this.signup = function(){
-
+  .service("authService",
+  ["$http", "localStorageService",
+  function($http, localStorageService){
+    this.signup = function(signupData, successCallback){
+      // signup data expects keys: name, email, password
+      $http.post("/api/v1/auth/signup", {
+        name: signupData.name,
+        email: signupData.email,
+        password: signupData.password
+      }).then(function(successResponse){
+        successCallback(successResponse);
+      }, function(failureResponse){
+      });
     };
-    this.login = function(){
-
+    this.login = function(loginData, successCallback){
+      // login data expects keys: email, password
+      $http.post("/api/v1/auth/login", {
+        email: loginData.email,
+        password: loginData.password
+      }).then(function(successResponse){
+        localStorageService.set("token", response.data.token);
+        successCallback(successResponse);
+      }, function(failureResponse){
+      });
     };
     this.logout = function(){
-
+      localStorageService.remove("token");
     };
     this.getCurrentToken = function(){
-
+      return localStorageService.get("token");
     }
-    this.logggedIn = function(){
-
+    this.loggedIn = function(){
+      if(localStorageService.get("token")){
+        return true;
+      }else{
+        return false;
+      }
     };
-  });
+  }]);
