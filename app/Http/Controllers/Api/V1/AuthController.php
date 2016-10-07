a4954743bd688b60bc4e0476b13e557d9af4862f
Angelo Lirazan <angelo.g.lirazan@gmail.com>
2016-10-07 14:37:52 -0700

    implement signup and login in api auth controller

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -7,14 +7,46 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+use App\User;
+use Auth;
+
 class AuthController extends Controller
 {
     //
     public function postSignup(Request $request){
-      return \Response::json("hello auth signup");
+      $newUser = User::create(array(
+        "name" => $request->input("name"),
+        "email" => $request->input("email"),
+        "password" => bcrypt($request->input("password")),
+      ));
+      if($newUser){
+        Auth::login($newUser, true); // 2nd param is "rememberable"
+        return \Response::json(array(
+          "token" => Auth::user()->getRememberToken()
+        ));
+      }else{
+        return \Response::json([
+          "code" => 400,
+          "message" => "User not created successfully."
+        ], 400);
+      }
     }
 
     public function postLogin(Request $request){
-      return \Response::json("hello auth login");
+      $loginData = array(
+        "email" => $request->input("email"),
+        "password" => $request->input("password")
+      );
+
+      if(Auth::attempt($loginData, true)){ // 2nd param is "rememberable"
+        return \Response::json(array(
+          "token" => Auth::user()->getRememberToken()
+        ));
+      }else{
+        return \Response::json([
+          "code" => 400,
+          "message" => "Login failed."
+        ], 400);
+      }
     }
 }
