c18beaeb8c3ec55ab1a14f1e0b3561f52f84d133
Angelo Lirazan <angelo.g.lirazan@gmail.com>
2016-10-12 02:05:40 -0700

    add method to main controller to grab appropriate user from the auth token

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,7 +8,19 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesResources;
 
+use App\User;
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
+
+    public function getCurrentTokenUserId($request){
+      $authToken = $request->header("auth-token");
+      if($authToken){
+        $authUser = User::where("remember_token", $authToken)->first();
+        return $authToken ? $authUser->id : false;
+      }else{
+        return false;
+      }
+    }
 }
