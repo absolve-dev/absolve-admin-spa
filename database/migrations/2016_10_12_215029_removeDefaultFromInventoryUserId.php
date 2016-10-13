a1e6261f4e33b3ba0884abfbe3d5240fb3c42050
Angelo Lirazan <angelo.g.lirazan@gmail.com>
2016-10-12 17:01:18 -0700

    add migration to add user_id to inventory in db

---
+++ database/migrations/2016_10_12_215029_removeDefaultFromInventoryUserId.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RemoveDefaultFromInventoryUserId extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+      Schema::table("inventories", function($table){
+        $table->integer("user_id")
+          ->default(null)
+          ->change();
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
