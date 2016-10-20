dc368fee248aa9fbbe44e8226fcaf1b811597f46
Angelo G Lirazan <angelo.g.lirazan@gmail.com>
2016-10-19 18:31:30 -0700

    implement breadcrumbs in all inventory parts

--- resources/assets/angular/admin/inventory/inventoryIndex.ctrl.js
+++ resources/assets/angular/admin/inventory/inventoryIndex.ctrl.js
@@ -1,12 +1,18 @@
 angular.module("absolve.admin.inventory")
   .controller("InventoryIndexController", [
-    "$scope", "inventoryService",
-    function($scope, inventoryService){
+    "$scope", "inventoryService", "breadcrumbsService",
+    function($scope, inventoryService, breadcrumbsService){
       $scope.inventories = false;
       var setInventories = function(inventoriesData){
         $scope.inventories = inventoriesData;
       };
 
       inventoryService.index(setInventories);
+
+      // make these juicy breadcrumbs
+      breadcrumbsService.pushBreadcrumb({
+        text: "Inventories",
+        link: adminInventoryUrlBasePath
+      });
     }
   ]);
