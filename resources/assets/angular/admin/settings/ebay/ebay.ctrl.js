ec717c0d3c0c0dfb90ba7be5423624f90addbc6e
Angelo G Lirazan <angelo.g.lirazan@gmail.com>
2016-10-25 12:35:07 -0700

    create front-end scaffolding, laravel functionality will be built off of this

--- resources/assets/angular/admin/settings/ebay/ebay.ctrl.js
+++ resources/assets/angular/admin/settings/ebay/ebay.ctrl.js
@@ -1,7 +1,23 @@
 angular.module("absolve.admin.settings.ebay")
   .controller("SettingsEbayController", [
-    "settingsEbayService", "breadcrumbsService",
-    function(settingsEbayService, breadcrumbsService){
+    "$scope", "$route", "settingsEbayService", "breadcrumbsService",
+    function($scope, $route, settingsEbayService, breadcrumbsService){
+      // initialize
+      var setEbaySettings = function(ebaySettingsData){
+        $scope.ebaySettings = ebaySettingsData;
+      }
+      settingsEbayService.get(setEbaySettings);
+
+      $scope.submitSave = function(){
+        // to be implemented
+        settingsEbayService.save(
+          $scope.ebaySettings,
+          function(){
+            // success callback
+            $route.reload();
+          });
+      };
+
       // make the damn breadcrumbs
       breadcrumbsService.pushBreadcrumb({
         text: "Settings",
--- resources/assets/angular/admin/settings/ebay/ebay.svc.js
+++ resources/assets/angular/admin/settings/ebay/ebay.svc.js
@@ -3,5 +3,10 @@ angular.module("absolve.admin.settings.ebay")
     "$http",
     function($http){
     // returns "this"
-
+    this.get = function(successCallback){
+      successCallback({auth_token:"hello"});
+    };
+    this.save = function(ebaySettingsData, successCallback){
+      successCallback();
+    };
   }]);
